5aadcf6a708679d92b912730bd42ca1bf2144c03
usnaik <upendra_naik@yahoo.com>
2019-08-21 15:03:41 +0530

    Initial commit

---
+++ limits-service/src/main/java/com/upen/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package com.upen.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.upen.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+}
